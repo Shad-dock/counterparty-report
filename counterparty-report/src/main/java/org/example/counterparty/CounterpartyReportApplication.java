0be804e54cc3c25b58f1bce939f1f3ed626a0ddf
Shad-dock <strangepear@mail.ru>
2026-05-17 13:48:47 +0300

    Add files via upload

---
+++ counterparty-report/src/main/java/org/example/counterparty/CounterpartyReportApplication.java
@@ -0,0 +1,13 @@
+package org.example.counterparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterpartyReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterpartyReportApplication.class, args);
+	}
+
+}
